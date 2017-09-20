886bb6c1780186867c42f236a71e9642862362e8
Jose <Jose@JOSECHO>
2017-09-20 14:06:08 -0600

    dsf

---
+++ lab2/Compresor.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rle
+{
+    public class Compresor
+    {
+        private string entrada;
+        private char[] Caracter;
+        private StringBuilder sb;
+
+        public string salida { get; set; }
+        
+        public Compresor(string entrada)
+        {
+            this.entrada = entrada;
+            Caracter = new char[entrada.Length];
+            Caracter = entrada.ToCharArray();
+            sb = new StringBuilder();
+            
+        }
+
+        public string Comprime()
+        {            
+            int i = 0;
+            int Iguales;
+            while (i < entrada.Length)
+            {
+                Iguales = IgualRepetido(Caracter[i], i);                
+                Salida(Iguales, Caracter[i]);
+                i += Iguales;
+                Iguales = 0;                
+            }
+            return sb.ToString();
+            Console.ReadLine();
+        }
+
+        private int Aux(char c, int i)
+        {            
+            char Auxj = Caracter[i++];
+            int cont = 1;
+            while (c == Auxj && i < entrada.Length)
+            {             
+                cont++;
+                Auxj = Caracter[i++];
+            }
+            return cont;
+        }
+
+        private int IgualRepetido(char c, int i)
+        {
+            int cont = 0;
+            while (c == Caracter[i])
+            {
+                cont++;
+                i++;
+                if (i == entrada.Length||cont == 5) break;
+            }
+            
+            return cont;
+        }
+        public string Descomprimir(string linea )
+        { 
+            
+            int i = 0;
+            int numero;
+            int j = 0;
+            string retorno = "";
+            while (i < linea.Length)
+                {
+                numero = Convert.ToInt32( Convert.ToString(linea[i]));
+                j = 1;
+                while(j <= numero)
+                {
+                    retorno = retorno + linea[i + 1];
+                    j++;
+                }
+                i+=2;
+                
+            }
+            return retorno;
+
+
+          }
+
+        private void Salida(int rep,char c)
+        {           
+            sb.Append(rep.ToString());
+            sb.Append(c);
+        }        
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using lab_5;
+using Rle;
+using lab_5.Huffman;
+using System.Collections;
+
+namespace lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el nombre del archivo");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese h si desea el metodo huffman o r para el metodo de compresion rle");
+            string opcion = Console.ReadLine();
+            Console.WriteLine("desea comprimir o descomprimir?");
+            StreamReader lector = new StreamReader(nombre);
+
+
+            string linea = lector.ReadLine();
+            string compresor = "";
+            while (linea != null)
+            {
+                compresor = compresor + linea;
+                linea = lector.ReadLine();
+            }
+            Compresor hola = new Compresor(compresor);
+            string guardar = Console.ReadLine();
+            HuffmanT huffmanTree = new HuffmanT();
+            // Huffman tree
+            List<Node> Lalista =  huffmanTree.Build(compresor);
+
+            // Encode
+            
+            if (guardar == "c")
+            {
+                BitArray encoded = huffmanTree.Encode(compresor);
+                if (opcion == "r")
+                {
+                    {
+                        int lineasalida;
+                        StreamWriter escritor = new StreamWriter(nombre + ".rlex");
+
+                        string lineaS = hola.Comprime();
+                        escritor.WriteLine(lineaS);
+                        lector.Close();
+                        lineasalida = compresor.Length - lineaS.Length;
+                        int prueba = compresor.Length;
+                        int prueba2 = lineaS.Length;
+                        escritor.Close();
+                        Console.WriteLine("Tamaño original : " + compresor.Length);
+                        Console.WriteLine("Tamaño Final : " + lineaS.Length);
+                        Console.WriteLine("ratio de compresion : " + Convert.ToString(Convert.ToDouble(lineaS.Length) / Convert.ToDouble(prueba)));
+                        Console.WriteLine("factor de compresion : " + Convert.ToString((Convert.ToDouble(prueba) / Convert.ToDouble(lineaS.Length))));
+                        Console.WriteLine(Convert.ToString(((Convert.ToDouble(prueba2) / (Convert.ToDouble(prueba))) + "%")));
+                    }
+                }
+                    StreamWriter escritor2 = new StreamWriter(nombre + ".chuffman");
+                    if (opcion == "h")
+                    {
+
+                        foreach (bool bit in encoded)
+                        {
+                            escritor2.Write((bit ? 1 : 0) + "");
+                        }
+                    int i = 0;
+                    while(i < Lalista.Count)
+                    {
+                       
+
+
+                    }
+                    }
+                    escritor2.Close();
+
+                }
+                if (guardar == "d")
+                {
+
+                    StreamWriter escritor2 = new StreamWriter(nombre + ".drlex");
+                    StreamWriter escritor = new StreamWriter(nombre + ".dhuffman");
+                    if (opcion == "r")
+                    {
+                        escritor2.WriteLine(hola.Descomprimir(compresor));
+                        lector.Close();
+                        escritor2.Close();
+                    }
+
+                    // Decode
+                    if (opcion == "h")
+
+                    {
+                    BitArray encoded = huffmanTree.Encode(compresor);
+                    string decoded = huffmanTree.Decode(encoded);
+
+                        escritor.WriteLine(decoded);
+
+                        escritor.Close();
+                    }
+
+
+                }
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+
+
